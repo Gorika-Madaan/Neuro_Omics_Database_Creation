7971ba3b1e0c752f942088d41acad0c98392ff9b
Gorika <142963299+Gorika-Madaan@users.noreply.github.com>
2024-08-07 12:45:36 +0000

    templates of all 4 done

--- app.py
+++ app.py
@@ -2,8 +2,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/") 
 #empty route for homepage
-def hello_world():
+def home():
     return render_template('home.html')
+
+@app.route("/genomics")
+def genomics():
+    return render_template('genomics.html')
+
+@app.route("/proteomics")
+def proteomics():
+    return render_template('proteomics.html')
+
+@app.route("/transcriptomics")
+def transcriptomics():
+    return render_template('transcriptomics.html')
+
+@app.route("/metabolomics")
+def metabolomics():
+    return render_template('metabolomics.html')
 
 print(__name__)
 if __name__ == "__main__":
