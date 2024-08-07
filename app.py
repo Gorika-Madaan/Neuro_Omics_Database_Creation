517fac1c08102b40450f6ff98f387fd2162b68ce
Gorika <142963299+Gorika-Madaan@users.noreply.github.com>
2024-08-07 06:24:41 +0000

    updated home.html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/") 
 #empty route for homepage
 def hello_world():
-    return "Hello World!"
+    return render_template('home.html')
 
 print(__name__)
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', debug=True)
+    app.run(host = '0.0.0.0', debug=True) 
